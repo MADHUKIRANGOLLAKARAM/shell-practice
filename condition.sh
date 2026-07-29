06ec58b5e817c20fe99dfe5c661e059273d93997
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-07-29 18:41:34 +0530

    changed

---
+++ condition.sh
@@ -0,0 +1,8 @@
+num=$1
+if [ $num -gt 18 ]; then
+   echo "you are eligible to vote"
+elif [ $num -eq 18 ]; then
+   echo "you have some more time to vote"
+else
+    echo "you are not eligible to vote..."
+fi
