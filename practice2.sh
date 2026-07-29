102f62be935965aadeeef1560c6b7b1c48dacf32
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-07-29 16:56:46 +0530

    added

--- practice2.sh
+++ practice2.sh
@@ -3,6 +3,6 @@
 start=$(date +%s)
 echo "script executed at $start"
 sleep 10
-end =$(date +%s)
-total =$(($end-$start))
+end=$(date +%s)
+total=$(($end-$start))
 echo "script executed in $total seconds."
