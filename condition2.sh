8235dc8aefd234e11332672e71eff954011db45f
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-07-30 11:04:52 +0530

    committed

--- condition2.sh
+++ condition2.sh
@@ -4,3 +4,12 @@ if [ $userid -ne 0 ]; then
     echo "you are not in root user environment.."
     exit 1
 fi
+
+echo "installing nginx..."
+dnf install nginx -y
+
+if [ $! -ne o ]; then
+    echo "installing nginx is failed..."
+else
+    echo "installing nginx is successfully...."
+fi
