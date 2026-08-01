ea074fe88470f413de8ad35248e6613adb1d1c40
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-01 12:15:21 +0530

    commited

--- package2.sh
+++ package2.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
 userid=$(id -u)
+Log_Folder="/var/log/shell-script"
+Log_File="/var/log/shell-script/$0.log"
 
 if [ $userid -ne 0 ]; then
     echo "you are not in root user environment please enter root environment..."
     exit 1
 fi
+
+mkdir -p $Log_Folder
 
 validate (){
 if [ $1 -ne 0 ]; then
@@ -15,13 +18,11 @@ else
 fi
 
 }
-dnf install nginx -y
+dnf install nginx -y &>> Log_File
 validate $? "installing nginx ..."
 
-dnf install mysql -y
+dnf install mysql -y &>> Log_File
 validate $? "installing mysql"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> Log_File
 validate $? "installing nodejs"
-
-
