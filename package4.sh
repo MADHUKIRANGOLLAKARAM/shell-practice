2a96f875eca75e87e374774ea7c9429de18650e0
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-01 13:47:07 +0530

    commited

---
+++ package4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+mkdir -p $LOG_FOLDER
+LOG-FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "you are not in root user environment.."
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ];then
+        echo "$2 failure..."
+    else 
+        echo "$2 success.."
+}
+
+dnf install nginx -y
+validate $? "installing nginx "
+
+dnf install mysql -y
+validate $? "installing mysql"
+
+dnf install nodejs -y
+validate $? "installing nodejs"
