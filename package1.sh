ee341ebff312f595257f2d3052ebc3293139778b
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-01 11:43:29 +0530

    commited

--- package1.sh
+++ package1.sh
@@ -7,11 +7,21 @@ if [ $userid -ne 0 ]; then
     exit 1
 fi
 
-echo "installing ..."
-dnf install nginx -y
-
-if [ $! -ne 0 ]; then
-    echo "install failed.."
+validate (){
+if [ $1 -ne 0 ]; then
+    echo "$2 failed.."
 else
-    echo "install succes."
+    echo "$2 succes."
 fi
+
+}
+dnf install nginx -y
+validate $? "installing nginx ..."
+
+dnf install mysql -y
+validate $? "installing mysql"
+
+dnf install nodejs -y
+validate $? "installing nodejs -y"
+
+
