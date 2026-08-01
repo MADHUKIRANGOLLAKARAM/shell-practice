da50b3ec0e26ae88762281f75b29fb007082b59b
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-01 11:36:46 +0530

    commited

---
+++ package1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ userid -ne 0 ]; then
+    echo "you are not in root user environment please enter root environment..."
+    exit 1
+fi
+
+echo "installing ..."
+dnf install nginx -y
+
+if [ $! -ne 0 ]; then
+    echo "install failed.."
+else
+    echo "install succes."
+fi
