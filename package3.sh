673cc2de20c95d3b45859a0dd948f205467ddda5
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-01 12:25:24 +0530

    commited

---
+++ package3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+userid=$(id -u)
+Log_Folder="/var/log/shell-script"
+Log_File="/var/log/shell-script/$0.log"
+
+if [ $userid -ne 0 ]; then
+    echo "you are not in root user environment please enter root environment..." | tee -a $Log_File
+    exit 1
+fi
+
+mkdir -p $Log_Folder
+
+validate (){
+if [ $1 -ne 0 ]; then
+    echo "$2 failed.." | tee -a $Log_File
+else
+    echo "$2 succes." | tee -a $Log_File
+fi
+
+}
+dnf install nginx -y &>> Log_File
+validate $? "installing nginx ..."
+
+dnf install cc -y &>> Log_File
+validate $? "installing cc"
+
+dnf install nodejs -y &>> Log_File
+validate $? "installing nodejs"
