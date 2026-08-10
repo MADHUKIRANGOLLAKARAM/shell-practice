820c7fdf732a988a4a8d36f1894b20d5b925790f
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-10 15:55:38 +0530

    commit

---
+++ backup1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+USER_ID=$(id -u)
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+DAYS=${3:-10}
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="$LOG_FOLDER/$(basename $0).log"
+
+if [ USER_ID -ne 0 ]; then
+    echo "you are not in root environment..."
+    exit 1
+fi
+
+usage(){
+    echo "source-dir destination-dir days"
+    
+}
+
+if [ $# -lt 2 ]; then
+    usage
+fi
+
+if [ ! -d SOURCE_DIR ]; then
+    echo "source directory is not exist : $SOURCE_DIR"
+fi
+
+if [ ! -d DESTINATION_DIR ]; then
+    echo "destination directory is not exist : $DESTINATION_DIR"
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR "*.log" -type f -mtime +10)
+
+
+while IFS= read -r filepath;
+do
+    echo "deleting a file : $filepath"
+    echo "deleted file: $filepath "
+
+done <<< $FILES_TO_DELETE
