e664ae50457b200fe0c76fe5f9e90b894f9a92ac
MADHUKIRAN GOLLAKARAM <gollakarammadhukiran@gmail.com>
2026-08-10 11:38:58 +0530

    commit

--- old_logs.sh
+++ old_logs.sh
@@ -3,11 +3,11 @@
 LOGS_DIR="/home/ec2-user/app-logs"
 LOG_FILE="$LOGS_DIR/$0.log"
 
-if [ ! $LOGS_DIR ]; then
+if [ ! -d $LOGS_DIR ]; then
     echo "logs dir not exist"
-    exit1
+    exit 1
 fi
 
-FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime +10)
+FILES_TO_DELETE=$(find "$LOGS_DIR" -name "*.log" -type f -mtime +10)
 
 echo "$FILES_TO_DELETE"
